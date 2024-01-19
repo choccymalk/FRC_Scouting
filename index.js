3b538f08cf156b2129a09188a3d12b98e3789429
ChoccyMalk <79180114+choccymalk@users.noreply.github.com>
2024-01-19 14:40:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -417,3 +417,41 @@ function eraseSurveys() {
     localStorage.surveys = "[]";
   }
 }
+
+function pingURL() { 
+  var URL = serverURL;
+  var settings = { 
+  
+    // Defines the configurations 
+    // for the request 
+    cache: false, 
+    dataType: "jsonp", 
+    async: true, 
+    crossDomain: true, 
+    url: URL, 
+    method: "GET", 
+    headers: { 
+      accept: "application/json", 
+      "Access-Control-Allow-Origin": "*", 
+    }, 
+  
+    // Defines the response to be made 
+    // for certain status codes 
+    statusCode: { 
+      200: function (response) { 
+        console.log("Status 200: Page is up!"); 
+      }, 
+      400: function (response) { 
+        console.log("Status 400: Page is down."); 
+      }, 
+      0: function (response) { 
+        console.log("Status 0: Page is down."); 
+      }, 
+    }, 
+  }; 
+  
+  // Sends the request and observes the response 
+  $.ajax(settings).done(function (response) { 
+    console.log(response); 
+  }); 
+} 
